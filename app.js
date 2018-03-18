3e1766e2759ff146cb68609ae05f120cbe56fbe6
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2018-03-18 18:44:16 +0700

    Configure Server for restfull API

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if(req.method === "OPTIONS")
+    {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
 
 const products = require('./api/routes/product');
 const orders = require('./api/routes/orders');
+
+mongoose.connect('mongodb://riskyferyansyah:akumakan2@project0-shard-00-00-wqide.mongodb.net:27017,project0-shard-00-01-wqide.mongodb.net:27017,project0-shard-00-02-wqide.mongodb.net:27017/test?ssl=true&replicaSet=Project0-shard-0&authSource=admin');
 
 app.use('/products', products);
 app.use('/orders', orders);
