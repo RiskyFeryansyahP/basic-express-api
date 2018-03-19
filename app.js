92bebcd3fd4b252846ef9c31f39ed43a4618efac
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2018-03-19 12:07:12 +0700

    configuring server for API

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended : false }));
 app.use(bodyParser.json());
 
@@ -21,11 +22,13 @@ app.use((req, res, next) => {
 
 const products = require('./api/routes/product');
 const orders = require('./api/routes/orders');
+const user = require('./api/routes/user');
 
-mongoose.connect('mongodb://riskyferyansyah:akumakan2@project0-shard-00-00-wqide.mongodb.net:27017,project0-shard-00-01-wqide.mongodb.net:27017,project0-shard-00-02-wqide.mongodb.net:27017/test?ssl=true&replicaSet=Project0-shard-0&authSource=admin');
+mongoose.connect('mongodb://riskyferyansyah:'+ process.env.MONGO_ATLAS_PW +'@project0-shard-00-00-wqide.mongodb.net:27017,project0-shard-00-01-wqide.mongodb.net:27017,project0-shard-00-02-wqide.mongodb.net:27017/test?ssl=true&replicaSet=Project0-shard-0&authSource=admin');
 
 app.use('/products', products);
 app.use('/orders', orders);
+app.use('/user', user);
 
 // Middleware for Handling Error
 app.use((req, res, next) => {
